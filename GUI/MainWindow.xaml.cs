30ac9e6d0ce010fe8a05605007aceda4df259ddf
edza <edexdev@gmail.com>
2016-02-22 19:45:04 +0200

    Added GUI

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Velleman8090;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private V8090 _relay;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+            this._relay = V8090.Instance;
+        }
+
+        private void Get(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(this.Index.Text.ToString());
+            this.Result.Text = this._relay.Get(index).ToString();
+        }
+
+        private void On(object sender, RoutedEventArgs e)
+        {
+            this.Result.Text = string.Empty;
+            int index = int.Parse(this.Index.Text.ToString());
+            this._relay.On(index);
+        }
+
+        private void Off(object sender, RoutedEventArgs e)
+        {
+            this.Result.Text = string.Empty;
+            int index = int.Parse(this.Index.Text.ToString());
+            this._relay.Off(index);
+        }
+
+        private void Toggle(object sender, RoutedEventArgs e)
+        {
+            this.Result.Text = string.Empty;
+            int index = int.Parse(this.Index.Text.ToString());
+            this._relay.Toggle(index);
+        }
+    }
+}
--- GUI/Properties/AssemblyInfo.cs
+++ GUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Velleman-8090-relay-CSharp-API-GUI")]
-[assembly: AssemblyDescription("github @edza")]
+[assembly: AssemblyTitle("GUI")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Velleman8090")]
+[assembly: AssemblyProduct("GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,6 +29,16 @@ using System.Windows;
 //the line below to match the UICulture setting in the project file.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
+)]
 
 
 // Version information for an assembly consists of the following four values:
